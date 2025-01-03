9cb3d863a3045af24d2ceacd0381aefdf67813fe
Jacob <120258067+frogbaseball@users.noreply.github.com>
2025-01-03 21:38:46 +0100

    final touches

--- YTVideoDownloader/Program.cs
+++ YTVideoDownloader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Spectre.Console;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -96,6 +97,7 @@ namespace YTVideoDownloader {
         static async Task DownloadVideoAsync() {
             var youtube = new YoutubeClient();
             var video = await youtube.Videos.GetAsync(url);
+            var videoName = CheckIfNameHasValidSymbols(video.Title);
             var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
             // audio
             var audioStreamInfo = streamManifest
@@ -106,27 +108,25 @@ namespace YTVideoDownloader {
             var videoStreamInfo = streamManifest
                 .GetVideoStreams()
                 .Where(s => s.Container == Container.Mp4)
-                .First(s => s.VideoQuality.Label == "480p");
+                .GetWithHighestVideoQuality();
             // mix audio and video streams
             var streamInfos = new IStreamInfo[] { audioStreamInfo, videoStreamInfo };
             AnsiConsole.WriteLine($"\nDownloading... (AUDIO: {audioStreamInfo.Size} VIDEO: {videoStreamInfo.Size}) \n");
-            await youtube.Videos.DownloadAsync(streamInfos, new ConversionRequestBuilder($"{video.Title}.mp4").SetFFmpegPath($"{Environment.CurrentDirectory}\\..\\..\\ffmpeg-windows-x64\\ffmpeg.exe").Build());
-            AnsiConsole.Markup($"[lime]Downloaded![/] in {path}\\{video.Title}.mp4 \n\n");
-
-            //TODO:
-            //Able to pick Video Quality that is available
-            //Ensure that the file name has symbols it supports
-            //Save the video to the set path
+            await youtube.Videos.DownloadAsync(streamInfos, new ConversionRequestBuilder($"{videoName}.mp4").SetFFmpegPath($"{Environment.CurrentDirectory}\\..\\..\\ffmpeg-windows-x64\\ffmpeg.exe").Build());
+            AnsiConsole.Markup($"[lime]Downloaded![/]\nin {path}\\{videoName}.mp4 \n\n");
+            //move file from the bin\debug folder to the desired path
+            File.Move($"{Environment.CurrentDirectory}\\{videoName}.mp4", $"{path}\\{videoName}.mp4");
         }
         static async Task DownloadAudioAsync() {
             var youtube = new YoutubeClient();
             var video = await youtube.Videos.GetAsync(url);
+            var videoName = CheckIfNameHasValidSymbols(video.Title);
             var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
             var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
             var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
             AnsiConsole.WriteLine($"\nDownloading... ({streamInfo.Size}) \n");
-            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{path}\\{video.Title}.mp3");
-            AnsiConsole.Markup($"[lime]Downloaded![/] in {path}\\{video.Title}.mp3 \n\n");
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{path}\\{videoName}.mp3");
+            AnsiConsole.Markup($"[lime]Downloaded![/]\nin {path}\\{videoName}.mp3\n\n");
         }
 
         static async Task SetPathAsync() {
@@ -146,6 +146,15 @@ namespace YTVideoDownloader {
                 FileData.SaveToFile(path);
                 await Menu();
             }
+        }
+        static string CheckIfNameHasValidSymbols(string name) {
+            char[] newName = name.ToCharArray();
+            for (int i = 0; i < newName.Length; i++) {
+                if (Path.GetInvalidFileNameChars().Contains(newName[i])) {
+                    newName[i] = 'X';
+                }
+            }
+            return new string(newName);
         }
         static void Input() {
             try {
